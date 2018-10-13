bdd76c89c7d8c462229c5bac4ca4fef1b0197d57
易林军 <1441457008@qq.com>
2018-10-14 00:45:24 +0800

    添加集线器

---
+++ Chat.Api/Hubs/ChatHub.cs
@@ -0,0 +1,60 @@
+﻿using Chat.Model.Entity.UserInfo;
+using Chat.Repository;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Chat.Api.Hubs
+{
+    /// <summary>
+    /// 聊天集线器
+    /// </summary>
+    public class ChatHub: Hub
+    {
+        /// <summary>
+        /// 在线用户连接Id => UserInfo 映射
+        /// </summary>
+        public static ConcurrentDictionary<string, UserInfo> OnlineClients { get; set; }
+        private UserInfoRepository _userInfoRepository = new UserInfoRepository();
+        private static readonly object SyncObj = new object();
+
+        static ChatHub()
+        {
+            OnlineClients = new ConcurrentDictionary<string, UserInfo>();
+        }
+
+        /// <summary>
+        /// 成功连接
+        /// </summary>
+        /// <returns></returns>
+        public override async Task OnConnectedAsync()
+        {
+            long userId = Convert.ToInt64(Context.GetHttpContext().Request.Query["UserId"]);
+            var user = _userInfoRepository.GetUserInfoByOpenId("", userId);
+            if(user!=null)
+            {
+                lock (SyncObj)
+                {
+                    OnlineClients[Context.ConnectionId] = user;
+                }
+            }
+            await base.OnConnectedAsync();
+        }
+
+        /// <summary>
+        /// 断开连接
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+
+            await base.OnDisconnectedAsync(exception);
+            lock (SyncObj)
+            {
+                OnlineClients.TryRemove(Context.ConnectionId, out UserInfo user);
+            }
+        }
+    }
+}
--- Chat.Api/Startup.cs
+++ Chat.Api/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Chat.Api.Hubs;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -36,7 +37,7 @@ namespace Chat.Api
                 //true表示生成控制器描述，包含true的IncludeXmlComments重载应放在最后，或者两句都使用true
                 options.IncludeXmlComments(xmlPath, true);
             });
-
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,6 +67,11 @@ namespace Chat.Api
                 action.RoutePrefix = string.Empty; ;
             });
 
+            //集线器
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chat");
+            });
         }
     }
 }
