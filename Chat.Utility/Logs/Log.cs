6a2c488872620c3d1cc1e6149676cb0179b0e4c0
易林军 <1441457008@qq.com>
2019-02-16 00:57:26 +0800

    add

---
+++ Chat.Utility/Logs/Log.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Logs
+{
+    public class Log
+    {
+
+    }
+
+    /// <summary>
+    /// 日志数据库操作
+    /// </summary>
+    public class LogRepository
+    {
+
+    }
+}
---
+++ Chat.Utility/Logs/LogEntity.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Logs
+{
+    /// <summary>
+    /// 日志实体类
+    /// </summary>
+    public class LogEntity
+    {
+        /// <summary>
+        /// 唯一主键
+        /// </summary>
+        public Guid LogId { get; set; }
+
+        /// <summary>
+        /// 日志级别
+        /// </summary>
+        public LogLevelEnum LogLevel { get; set; }
+
+        /// <summary>
+        /// 事务号
+        /// </summary>
+        public Guid TransactionID { get; set; }
+
+        /// <summary>
+        /// 用户Id
+        /// </summary>
+        public int? UId { get; set; }
+
+        /// <summary>
+        /// 平台（小程序miniApp，android,ios,浏览器browser,h5)
+        /// </summary>
+        public string Platform { get; set; }
+
+        /// <summary>
+        /// 日志标题
+        /// </summary>
+        public string LogTitle { get; set; }
+
+        /// <summary>
+        /// 日志内容
+        /// </summary>
+        public string LogContent { get; set; }
+    }
+
+    /// <summary>
+    /// 日志标签（供日志精准筛查）
+    /// </summary>
+    public class LogTag
+    {
+        /// <summary>
+        /// 唯一主键
+        /// </summary>
+        public Guid TagId { get; set; }
+
+        /// <summary>
+        /// 日志主表Id
+        /// </summary>
+        public Guid LogId { get; set; }
+
+        /// <summary>
+        /// 键
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// 值
+        /// </summary>
+        public string Value { get; set; }
+    }
+
+    /// <summary>
+    /// 日志级别
+    /// </summary>
+    public enum LogLevelEnum
+    {
+        Debug=0,
+        Info=1,
+        Warn=2,
+        Error=3,
+        Fatal=4
+    }
+}
