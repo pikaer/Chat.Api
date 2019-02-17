4c7b2e438e60eb76fb9a896ce2f86fc429c584e2
易林军 <1441457008@qq.com>
2019-02-17 13:13:43 +0800

    add

--- Chat.Interface/IChatInterface.cs
+++ Chat.Interface/IChatInterface.cs
@@ -5,6 +5,7 @@ namespace Chat.Interface
     /// <summary>
     /// 前端接口集合，每个控制器请求的接口都在这里汇集
     /// 目的：使得接口更加清晰明了，规范前端交互所需类和服务端代码所需类
+    /// 强制要求：前端请求类：XXXXRequest，前端响应类：XXXXResponse,都放在Chat.Model.Utils.ChatApi下面
     /// </summary>
     public interface IChatInterface
     {
